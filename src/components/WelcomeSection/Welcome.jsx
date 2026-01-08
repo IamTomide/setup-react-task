b0002acf7814b24dd213d1a8b99367c747309aba
Olaleye Oluwatomide <paulinaebunoluwa@gmail.com>
2026-01-08 11:38:08 +0100

    adjusted hero section and about us section

--- src/components/WelcomeSection/Welcome.jsx
+++ src/components/WelcomeSection/Welcome.jsx
@@ -9,16 +9,19 @@ import FloatingImage from '../floatingImages/FloatingImage';
 const Welcome = () => {
     return ( 
         <section className='introduction'>
-            <Introduction />
-            <FloatingImage src={bookImg} style={{top: '458.05px', left: '94.64px'}}/>
-            <FloatingImage src={curvedArrow} style={{top: '443.81px', left: '551.86px'}}/>
-            <FloatingImage src={dotsBg} style={{top: '147.29px', left: '1103.2px', zIndex: '1'}}/>
+            <div className="intro-container">
+                <Introduction />
+                <FloatingImage src={bookImg} style={{top: '458.05px', left: '94.64px'}}/>
+                <FloatingImage src={curvedArrow} style={{top: '443.81px', left: '551.86px'}}/>
+                <FloatingImage src={dotsBg} style={{top: '147.29px', left: '1103.2px', zIndex: '1'}}/>
 
-            <div className='introduction__instructors'>
-                <p>200+ <span>Instructor</span></p>
-                <img src={instructors} alt="instructurs-image" />
+                <div className='introduction__instructors'>
+                    <p>200+ <span>Instructor</span></p>
+                    <img src={instructors} alt="instructurs-image" />
+                </div>
+            
             </div>
-           
+
         </section>
      );
 }
